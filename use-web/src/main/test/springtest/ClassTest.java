c4b53b4a998d9343d33514888eec9e2ca85b1eaf
gent-chen <1058414767@qq.com>
2020-05-13 17:34:14 +0800

    add ServiceLoader test

--- use-web/src/main/test/springtest/ClassTest.java
+++ use-web/src/main/test/springtest/ClassTest.java
@@ -6,6 +6,8 @@ import spring.tomcat.ServeltImpl;
 
 import javax.servlet.ServletException;
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.ServiceLoader;
 
 public class ClassTest {
 
@@ -14,5 +16,18 @@ public class ClassTest {
         System.out.println(WebApplicationContext.class.getName() + ".ROOT");
         ServeltImpl impl=new ServeltImpl();
         impl.service(null,null);
+    }
+
+    @Test
+    public void hibernateTest(){
+        ServiceLoader<AutoCloseable> serviceLoader = ServiceLoader.load( AutoCloseable.class );
+        int count=0;
+        Iterator<AutoCloseable> iterator=serviceLoader.iterator();
+        while(iterator.hasNext()){
+            count++;
+            iterator.next();
+        }
+
+        System.out.println(count);
     }
 }
